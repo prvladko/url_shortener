12f94fe7e09f52de2575a773112f17e6b3c114d6
prvladko <prvladko@gmail.com>
2022-09-19 10:14:59 +0300

    Context (prepare functions that are necessary for shortener): - Find a short link record by <key> - Generate a random short string - Assign a random string on the <key> column - Increment <hit_count>

--- lib/shorty/short_links.ex
+++ lib/shorty/short_links.ex
@@ -7,6 +7,7 @@ defmodule Shorty.ShortLinks do
   alias Shorty.Repo
 
   alias Shorty.ShortLinks.ShortLink
+  # alias Shorty.ShortLinks.PubSub # TODO: uncomment later
 
   @doc """
   Returns the list of short_links.
@@ -17,9 +18,9 @@ defmodule Shorty.ShortLinks do
       [%ShortLink{}, ...]
 
   """
-  def list_short_links do
-    Repo.all(ShortLink)
-  end
+  # def list_short_links do
+    # Repo.all(ShortLink)
+  # end
 
   @doc """
   Gets a single short_link.
@@ -35,7 +36,8 @@ defmodule Shorty.ShortLinks do
       ** (Ecto.NoResultsError)
 
   """
-  def get_short_link!(id), do: Repo.get!(ShortLink, id)
+  # def get_short_link!(id), do: Repo.get!(ShortLink, id)
+  def get_short_link_by_key(key), do: Repo.get_by(ShortLink, key: key) # Find a short link record by key
 
   @doc """
   Creates a short_link.
@@ -50,9 +52,33 @@ defmodule Shorty.ShortLinks do
 
   """
   def create_short_link(attrs \\ %{}) do
+    attrs = maybe_assign_random_key(attrs)
+
     %ShortLink{}
     |> ShortLink.changeset(attrs)
     |> Repo.insert()
+    # |> PubSub.broadcast_record(:short_link_inserted) # TODO: uncomment later
+  rescue
+    # Retry in case the same key already exists in database
+    Ecto.ConstraintError ->
+      create_short_link(attrs)
+  end
+
+
+  # Generates and assigns a random key when key is blank.
+  defp maybe_assign_random_key(attrs) do
+    if attrs["key"] in [nil, ""] do
+      Map.put(attrs, "key", random_string(4))
+    else
+      attrs
+    end
+  end
+
+  defp random_string(length) when is_integer(length) do
+    :crypto.strong_rand_bytes(length)
+    |> Base.url_encode64()
+    |> String.replace(~r/[-_\=]/, "")
+    |> binary_part(0, length)
   end
 
   @doc """
@@ -71,6 +97,12 @@ defmodule Shorty.ShortLinks do
     short_link
     |> ShortLink.changeset(attrs)
     |> Repo.update()
+    # |> PubSub.broadcast_record(:short_link_updated) # TODO: uncomment later
+  end
+
+
+  def increment_hit_count(short_link) do
+    update_short_link(short_link, %{hit_count: short_link.hit_count + 1})
   end
 
   @doc """
@@ -87,6 +119,7 @@ defmodule Shorty.ShortLinks do
   """
   def delete_short_link(%ShortLink{} = short_link) do
     Repo.delete(short_link)
+    # |> PubSub.broadcast_record(:short_link_deleted) # TODO: uncomment later
   end
 
   @doc """
