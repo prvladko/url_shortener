deac9e6a216770db6d06372ca5eac8a2dcf97bbd
prvladko <prvladko@gmail.com>
2022-09-19 09:19:46 +0300

    Improve the content of the migration file: - Add not-null constraint on :key and :url columns - Add default value 0 on :hit_count column - Add unique constraint on :key column

--- priv/repo/migrations/20220919061053_create_short_links.exs
+++ priv/repo/migrations/20220919061053_create_short_links.exs
@@ -3,11 +3,13 @@ defmodule Shorty.Repo.Migrations.CreateShortLinks do
 
   def change do
     create table(:short_links) do
-      add :" key", :string
-      add :" url", :text
-      add :" hit_count", :integer
+      add :" key", :string, null: false # Add not-null constraint
+      add :" url", :text, null: false
+      add :" hit_count", :integer, default: 0 #Add default value 0
 
       timestamps()
     end
+
+    create index(:short_links, [:key], unique: true)
   end
 end
