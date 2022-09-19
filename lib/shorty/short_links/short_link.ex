14b1a84c6338db51f476f1d40afb8c68856c40b7
prvladko <prvladko@gmail.com>
2022-09-19 09:37:16 +0300

    Schema: - used ecto_fields package for URL validation

--- lib/shorty/short_links/short_link.ex
+++ lib/shorty/short_links/short_link.ex
@@ -3,9 +3,9 @@ defmodule Shorty.ShortLinks.ShortLink do
   import Ecto.Changeset
 
   schema "short_links" do
-    field :" hit_count", :integer
+    field :" hit_count", :integer, default: 0
     field :" key", :string
-    field :" url", :string
+    field :" url", EctoFields.URL # used ecto_fields package for URL validation
 
     timestamps()
   end
@@ -14,6 +14,6 @@ defmodule Shorty.ShortLinks.ShortLink do
   def changeset(short_link, attrs) do
     short_link
     |> cast(attrs, [:" key", :" url", :" hit_count"])
-    |> validate_required([:" key", :" url", :" hit_count"])
+    |> validate_required([:url])
   end
 end
--- mix.exs
+++ mix.exs
@@ -47,7 +47,8 @@ defmodule Shorty.MixProject do
       {:telemetry_poller, "~> 1.0"},
       {:gettext, "~> 0.18"},
       {:jason, "~> 1.2"},
-      {:plug_cowboy, "~> 2.5"}
+      {:plug_cowboy, "~> 2.5"},
+      {:ecto_fields, "~> 1.3.0"}
     ]
   end
 
